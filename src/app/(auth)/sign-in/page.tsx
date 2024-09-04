2db9baacf5e1524d87e9cdf4164d9c747a295d8b
ddas4548 <ddas4548@gmail.com>
2024-09-04 22:09:38 +0530

    sign in ui part completed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,129 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default function comp() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import axios, { AxiosError } from "axios";
+
+import {
+  Form,
+  FormControl,
+  
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { signInSchema } from "@/schemas/signinSchema";
+import { signIn } from "next-auth/react";
+
+export default function Page() {
+ 
+ 
+  const { toast } = useToast();
+  const router = useRouter();
+  
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      
+      identifier: "",
+      password: "",
+    },
+  });
+
+ 
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+   const result = await signIn('credentials', {
+      redirect:false,
+      identifier: data.identifier,
+      password: data.password,
+    })
+    if(result?.error){
+      toast({
+        title: "log in failed",
+        description: "incorrect username or password",
+        variant: "destructive"
+      })
+    }
+    if(result?.url){
+      router.replace("/dashboard")
+    }
+  };
+
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className=" flex justify-center items-center min-h-screen bg-gray-200">
+        <div className=" w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+          <div className=" text-center">
+            <h1 className=" text-2xl text-orange-600 font-extrabold tracking-tight lg:text-3xl mb-6">
+              Join Mystery Box
+            </h1>
+            <p className=" mb-4"> Sign in to start your anonymous adventure</p>
+          </div>
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                
+                <FormField
+                  name="identifier"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email/Username</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="email/username"
+                          {...field}
+                          
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                 <FormField
+                  name="password"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="password"
+                          {...field}
+                          
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" >
+                  Sign in
+                  </Button>
+              </form>
+            </Form>
+            
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
